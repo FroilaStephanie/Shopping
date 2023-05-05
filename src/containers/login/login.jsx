6b27c23b05296dd94f4c6f6281e73e49b0f094b2
FroilaStephanie <froila0301@gmail.com>
2023-05-05 22:19:40 +0530

    hi

--- src/containers/login/login.jsx
+++ src/containers/login/login.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  
   return (
-    <div>
-      Login Page
-    </div>
+    <form>
+      <label for="email">email </label>
+      <input 
+      value= {email}
+       onChange={(e) => 
+       setEmail(e.target.value)}
+       type="email" placeholder="youremail@gmail.com" id="email" name="email"></input>
+      <label for="password">password </label>
+      <input value= {pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="********" id="password" name="password"></input>
+      <div>{email}{pass}</div>
+      <button>LOGIN</button>
+    </form>
   )
 };
 
