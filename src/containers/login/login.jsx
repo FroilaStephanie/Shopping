cbb19119c80121b2776c0645eb00126a7d51076b
Kabilesh02799 <95333434+Kabilesh02799@users.noreply.github.com>
2023-05-05 22:51:17 +0530

    login scss

--- src/containers/login/login.jsx
+++ src/containers/login/login.jsx
@@ -1,20 +1,37 @@
 import React,{ useState } from 'react';
+import './style.css';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
   
   return (
-    <form>
-      <label for="email">email </label>
-      <input 
-      value= {email}
-       onChange={(e) => 
-       setEmail(e.target.value)}
-       type="email" placeholder="youremail@gmail.com" id="email" name="email"></input>
-      <label for="password">password </label>
-      <input value= {pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="********" id="password" name="password"></input>
-      <div>{email}{pass}</div>
+    <form className='login-form'>
+      <div className='email-wrapper'>
+        <label for="email">
+          email
+        </label>
+        <input
+          value= {email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="youremail@gmail.com"
+          id="email"
+          name="email"
+          className='form-email'
+        />
+      </div>
+      <div className='password-wrapper'>
+        <label for="password">password </label>
+        <input
+          value= {pass}
+          onChange={(e) => setPass(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+        />
+      </div>
       <button>LOGIN</button>
     </form>
   )
